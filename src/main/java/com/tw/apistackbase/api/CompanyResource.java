43985d9254f15c5d3cf824f615ebd6cc0af39179
Teng YANG <teng.yang@oocl.com>
2019-07-17 14:43:07 +0800

    feat: implement Company delete

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -27,4 +27,9 @@ public class CompanyResource {
     public Company update(@RequestBody Company company) {
         return companyRepository.save(company);
     }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        companyRepository.deleteById(id);
+    }
 }
