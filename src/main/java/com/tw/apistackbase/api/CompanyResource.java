e528733ae578990a74b92033a7e861c945badae8
Teng YANG <teng.yang@oocl.com>
2019-07-17 14:34:44 +0800

    feat: implement Company create update and list

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,30 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
-    
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return companyRepository.findAll();
     }
-    
+
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
+    }
+
+    @PutMapping
+    public Company update(@RequestBody Company company) {
+        return companyRepository.save(company);
     }
 }
